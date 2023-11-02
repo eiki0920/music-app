901062bef3ec0ac33cd3ed8a7496289d9919f0ac
eiki0920 <s2122039@stu.musashino-u.ac.jp>
2023-11-02 22:32:48 +0900

    アクセストークンの更新とプレイリスト取得

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from "react";
+
 function Home() {
+  const [token, setToken] = useState("");
+  const [playList, setPlaylist] = useState({});
+
+  useEffect(() => {
+    fetch(`https://accounts.spotify.com/api/token`, {
+      method: "POST",
+      headers: {
+        Authorization:
+          "Basic " +
+          btoa(
+            `${process.env.REACT_APP_API_CLIENT}:${process.env.REACT_APP_API_CLIENT_SECRET}`
+          ),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: process.env.REACT_APP_API_REFRESH_TOKEN,
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((tokenData) => {
+        setToken(tokenData.access_token);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://api.spotify.com/v1/playlists/37i9dQZEVXbKXQ4mDTEBXq`, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    })
+      .then((res) => {
+        if (res.status === 400) {
+          throw new Error("アクセストークンが違う");
+        }
+        return res.json();
+      })
+      .then((playlist) => {
+        console.log(playlist);
+        setPlaylist(playlist);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        console.log("token", token);
+      });
+  }, [token]);
+
   return (
     <>
       <h1>Home</h1>
+      <h1>{playList.description}</h1>
     </>
   );
 }
--- src/pages/detailArtist.jsx
+++ src/pages/detailArtist.jsx
@@ -1,7 +1,52 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const token = process.env.REACT_APP_API_TOKEN;
+
 function ArtistDetail() {
+  const params = useParams();
+  const [artistData, setArtistData] = useState({});
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_ENDPOINT}/artists/${params.artist_id}`, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((artist) => {
+        console.log(artist);
+        console.log(artist.images[0].url);
+        setArtistData(artist);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [params.artist_id]);
+
   return (
     <>
       <h2>アーティスト詳細ページ</h2>
+      <li>{artistData.name}</li>
+      <h2>{artistData.type}</h2>
+      {artistData.followers && (
+        <li>フォロワー: {artistData.followers.total}</li>
+      )}
+
+      {artistData.images && (
+        <div className="artist-image">
+          <img src={artistData.images[0].url} alt="artist-img" />
+        </div>
+      )}
+
+      {/* <div className="artist-description">
+          <ul>
+            <li>{artistData.name}</li>
+            <li>フォロワー: {artistData.followers.total}</li>
+          </ul>
+          <Link to={artistData.external_urls.spotify}>spotifyで聞いてみる</Link>
+        </div> */}
     </>
   );
 }
